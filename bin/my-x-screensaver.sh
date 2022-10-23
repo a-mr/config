4854d0ddd21c1971c27817c1b5141cac88609f62
Andrey Makarov <ph.makarov@gmail.com>
2022-10-23 18:50:07 +0300

    add screensaver script

---
+++ bin/my-x-screensaver.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SWITCH_TIMEOUT=600  # turn the screen off in 10 minutes
+BASE_TIMEOUT=$((SWITCH_TIMEOUT/2))
+
+while true; do
+    t=`xprintidle`
+    t=$((t/1000))
+    #echo $t
+    if [ $t -gt $SWITCH_TIMEOUT ]; then
+        if [ -d ~/nfs/s ]; then  # home computer
+            xset dpms force off
+        else                     # work computer
+            # light-locker-command -l
+            xflock4
+        fi
+    fi
+    sleep $BASE_TIMEOUT
+done
