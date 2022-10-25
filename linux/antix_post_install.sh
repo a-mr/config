af957d93ab3580fabeeca4e7bb1c282d19cbf7bc
Andrey Makarov <ph.makarov@gmail.com>
2022-10-25 22:53:21 +0300

    Minor changes

--- linux/antix_post_install.sh
+++ linux/antix_post_install.sh
@@ -3,3 +3,6 @@
 for group in vboxsf vboxusers dialout docker; do
     sudo usermod -a -G $group $USER
 done
+
+echo $'Section "ServerFlags"\n  Option "DontZap" "yes"\nEndSection' \
+    | sudo tee /usr/share/X11/xorg.conf.d/dontzap.conf
