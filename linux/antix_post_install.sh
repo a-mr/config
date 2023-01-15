0ff77f8583fb29d9ed95dd5db20c15d85d9cc1c1
Andrey Makarov <ph.makarov@gmail.com>
2023-01-15 13:02:19 +0300

    minor addition

--- linux/antix_post_install.sh
+++ linux/antix_post_install.sh
@@ -6,3 +6,7 @@ done
 
 echo $'Section "ServerFlags"\n  Option "DontZap" "yes"\nEndSection' \
     | sudo tee /usr/share/X11/xorg.conf.d/dontzap.conf
+
+sudo echo kernel.yama.ptrace_scope = 0 > /etc/sysctl.d/10-ptrace.conf
+
+cp 60-ioschedulers.rules /etc/udev/rules.d/
